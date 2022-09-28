9c02a2ac189d9fd53d1587636d5be2d627674807
Khiem Thanh Phung <khiemphung@Khiems-MBP.attlocal.net>
2022-09-28 14:50:20 -0700

    reinstalled npm inquirer@8.2.4

---
+++ index.js
@@ -0,0 +1,26 @@
+const inquirer = require("inquirer");
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: "username",
+        message: "What is your GitHub?"
+    },
+    {
+        type: 'input',
+        name: "username",
+        message: "What is your GitHub?"
+    },    {
+        type: 'input',
+        name: "username",
+        message: "What is your GitHub?"
+    },    {
+        type: 'input',
+        name: "username",
+        message: "What is your GitHub?"
+    },    {
+        type: 'input',
+        name: "username",
+        message: "What is your GitHub?"
+    },
+])
