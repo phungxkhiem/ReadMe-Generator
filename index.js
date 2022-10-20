1bd2ccd73f82dc47fd14da2525eecb23a71fa937
Khiem Thanh Phung <khiemphung@khiems-mbp.attlocal.net>
2022-10-19 19:27:31 -0700

    question

--- index.js
+++ index.js
@@ -2,38 +2,86 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./generateMarkdown")
 
-inquirer.prompt([
+const questions = [
     {
-        type: 'input',
-        name: "username",
-        message: "What is your GitHub?"
-    },
-    {
-        type: 'input',
-        name: "repo",
-        message: "What is the name of your GitHub repo?"
-    },    {
-        type: 'input',
+        type: "input",
         name: "title",
-        message: "What is the title of your project?"
-    },    {
-        type: 'input',
-        name: "description",
-        message: "Write a description of your project?"
-    },    {
-        type: 'list',
-        name: "license",
-        message: "Which license did you use? (choices all that applies",
-        choices: ['MIT', 'something', 'something']
+        message: "What is the title of your project? (Required)",
+        validate: titleInput => {
+          if (titleInput) {
+            return true;
+          } else {
+            console.log("Please enter your project title.");
+            return false;
+          }
+        }
+      },
+      {
+        type: "input",
+        name: "projectDescription",
+        message: "Briefly give an explanation of your project. (Required)",
+        validate: description => {
+          if (description) {
+            return true;
+          } else {
+            console.log("Please write a short description.");
+            return false;
+          }
+        }
+      },
+      {
+        type: "input",
+        name: "installInfo",
+        message: "Provide any installation instructions here:",
+      },
+      {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Which license is used for this project:',
+        choices: ['Apache', 'Mozilla', 'MIT', 'GNU', 'Boost', 'ISC'],
+        validate: choicesLength => {
+          if (choicesLength.length <= 1) {
+            return true;
+          } else {
+            return "Please select one license!";
+          }
+        }
+      },
+      {
+        type: "input",
+        name: "usageInfo",
+        message: "Describe the main use case for this project/application:",
+      },
+      {
+        type: "input",
+        name: "contributionsInfo",
+        message: "Are their any guidlines for contributing to this project:",
+      },
+      {
+        type: "input",
+        name: "testsInfo",
+        message: "Which tests would you like to include:",
+      },
+      {
+        type: "input",
+        name: "githubUsername",
+        message: "What is your GitHub username (Required)?",
+      },
+      {
+        type: "input",
+        name: "emailInfo",
+        message: "What is a good email someone can reach you(Required)?",
+      }
+];
 
-    },
-])
 
-.then((data) => {
-    fs.writeFile("readMeGenerator.md", JSON.stringify(data, null, `\t`), (error)=>{
-        error ? console.log(error) : console.log("success")
-    })
-})
-
+function writeToFile(fileName, data) {
+    fs.writeFile("./README.md", generateMarkdown(data), function(err) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log('Success!');
+      });
+}
 
 answer.name
